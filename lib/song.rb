75348e60923283ea6c5f70d672399aa90adfa95a
Emily Wessel <ettl02@gmail.com>
2018-07-02 01:06:55 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,18 +27,6 @@ def self.genres
   @@genres.uniq
 end
 
-def self.genre_count
-  genre_count = {}
-  @@genres.each do |genre|
-    if genre.count[genre]
-      genre_count[genre] += 1
-    else
-      genre_count[genre] = 1
-    end
-  end
-  genre_count
-end
-
 def self.artist_count
   artist_count = {}
   @@artists.each do |artist|
@@ -49,5 +37,17 @@ def self.artist_count
   end
 end
 artist_count
+end
+
+def self.genre_count
+  genre_count = {}
+  @@genres.each do |genre|
+    if genre.count[genre]
+      genre_count[genre] += 1
+    else
+      genre_count[genre] = 1
+    end
+  end
+  genre_count
 end
 end
